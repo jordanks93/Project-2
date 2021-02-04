eef6485f02abb64d68981f161d332cc5c6e0a887
Thomas Whittle <tewhittle@gmail.com>
2021-02-04 18:53:31 -0500

    Add email page with styling

--- public/js/mail.js
+++ public/js/mail.js
@@ -1,12 +1,12 @@
 const mailgun = require("mailgun-js");
-const DOMAIN = "sandbox41529fa617c14faead62eb4a228b69d0.mailgun.org";
-const mg = mailgun({apiKey: "95284ed979f11a0984399d92a5a3c942-77751bfc-7d676c2b", domain: DOMAIN});
+const DOMAIN = "";
+const mg = mailgun({apiKey: "", domain: DOMAIN});
 const path = require("path")
 
-const filepath = path.join(__dirname, "verstappen.jpg")
+const filepath = path.join(__dirname, "")
 
 const data = {
-	from: "Mailgun Sandbox <postmaster@sandbox41529fa617c14faead62eb4a228b69d0.mailgun.org>",
+	from: "Mailgun Sandbox <>",
 	to: "tewhittle@protonmail.com",
     subject: "Hello",
 	html: "<h1>HTML in an email!</h1><br><h2>Easier than I thought</h2>",
