3ad0261fae8db5a20168f01f11524f423b860455
Thomas Whittle <tewhittle@gmail.com>
2021-02-04 19:13:02 -0500

    Add html file for email with styling

--- public/js/mail.js
+++ public/js/mail.js
@@ -1,6 +1,6 @@
 const mailgun = require("mailgun-js");
-const DOMAIN = "";
-const mg = mailgun({apiKey: "", domain: DOMAIN});
+const DOMAIN = "https://api.mailgun.net/v3/sandbox41529fa617c14faead62eb4a228b69d0.mailgun.org";
+const mg = mailgun({apiKey: "key-127f13986c8e9a5611e8a85450131665", domain: DOMAIN});
 const path = require("path")
 
 const filepath = path.join(__dirname, "")
@@ -9,8 +9,7 @@ const data = {
 	from: "Mailgun Sandbox <>",
 	to: "tewhittle@protonmail.com",
     subject: "Hello",
-	html: "<h1>HTML in an email!</h1><br><h2>Easier than I thought</h2>",
-	attachment: filepath,
+	html: ,
 	text: "Testing some Mailgun awesomness!"
 };
 mg.messages().send(data, function (error, body) {
