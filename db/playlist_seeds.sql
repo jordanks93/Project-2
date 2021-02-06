56944f7cf314d6ba19a8d590d3bb5d636932cf8c
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-02-06 13:52:59 -0500

    added more seeds for songs

--- db/playlist_seeds.sql
+++ db/playlist_seeds.sql
@@ -23,6 +23,12 @@ VALUES ("Whole Lotta Love", "Led Zepplin", "Led Zepplin II", 1969, "Rock", 1, CU
 INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
 VALUES ("Sweet Emotion", "Aerosmith", "Toys In The Attic", 1975, "Rock", 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
+INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
+VALUES ("Paradise City", "Guns N' Roses", "Appetite For Destruction", 1987, "Rock", 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
+VALUES ("Fortunate Son", "Creedence Clearwater Revival", "Willy And The Poor Boys", 1969, "Rock", 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
 
 /*Rap Playlist*/
 INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
@@ -34,6 +40,12 @@ VALUES ("PRIDE.", "Kendrick Lamarr", "DAMN.", 2017, "Rap", 2, CURRENT_TIMESTAMP,
 INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
 VALUES ("Faucet Failure", "Ski Mask The Slump God", "STOKELEY", 2018, "Rap", 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
+INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
+VALUES ("Self Care", "Mac Miller", "Swimming", 2018, "Rap", 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
+VALUES ("No Option", "Post Malone", "Stoney", 2016, "Rap", 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
 
 /*Alt Playlist*/
 INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
@@ -44,6 +56,12 @@ VALUES ("Slumlord", "Neon Indian", "VEGA INTL. Night School", 2015, "Alternative
 
 INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
 VALUES ("Breathe Deeper", "Tame Impala", "The Slow Rush", 2020, "Alternative", 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
+VALUES ("Under The Moon", "Foster The People", "In The Darkest Of Nights, Let The Birds Sing", 2020, "Alternative", 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO songs (song_name, artist, album, year, genre, PlaylistId, createdAt, updatedAt) 
+VALUES ("Another Rise, Another Fall", "Miami Horror", "All Possible Futures", 2015, "Alternative", 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 SELECT * FROM Playlists;
 
