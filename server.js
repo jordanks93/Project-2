5011788c5580697bd433f936a9b7a6b093edfdd9
Adam L <alane019@gmail.com>
2021-02-06 01:54:20 -0500

    add handlebars setup and sequelize force sync to false

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 const db = require('./models');
 
+//Set Handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,9 +22,9 @@ app.use(express.static('public'));
 
 // Routes
 require('./routes/api_routes.js')(app);
-// require('./routes/html-routes.js')(app);
+;
 
 // Syncing our sequelize models and then starting our Express app
-db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+db.sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`\n Listening on PORT ${PORT} (http://localhost:${PORT})`));
 });
